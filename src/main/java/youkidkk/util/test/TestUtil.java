7f9e2a2a319f9155a76d14655b98a3e87176743b
youkidkk <youkidkk80@gmail.com>
2016-12-17 21:19:15 +0900

    戻り値なしのメソッドを追加

--- src/main/java/youkidkk/util/test/TestUtil.java
+++ src/main/java/youkidkk/util/test/TestUtil.java
@@ -40,6 +40,34 @@ public class TestUtil {
     }
 
     /**
+     * 戻り値のないprivateメソッドを呼び出す。
+     *
+     * @param targetClass 呼び出し対象クラス
+     * @param targetObject 呼び出し対象オブジェクト
+     * @param methodName 呼び出し対象メソッド名
+     * @param args 引数リスト
+     * @param argClasses 引数の型リスト
+     * @throws SecurityException セキュリティ・マネージャの例外
+     * @throws NoSuchMethodException 一致するメソッドが見つからない場合
+     * @throws InvocationTargetException 基本となるメソッドが例外をスローする場合
+     * @throws IllegalArgumentException メソッド引数異常の場合
+     * @throws IllegalAccessException メソッドアクセス異常の場合
+     */
+    public static void invokePrivateVoidMethod(
+            Class<?> targetClass,
+            Object targetObject,
+            String methodName,
+            List<Object> args,
+            List<Class<?>> argClasses)
+            throws NoSuchMethodException, SecurityException,
+            IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        Method method = targetClass.getDeclaredMethod(methodName,
+                argClasses.toArray(new Class[0]));
+        method.setAccessible(true);
+        method.invoke(targetObject, args.toArray(new Object[0]));
+    }
+
+    /**
      * private-staticメソッドを呼び出す。
      *
      * @param clazz 呼び出し対象クラス
@@ -64,6 +92,31 @@ public class TestUtil {
         Method method = clazz.getDeclaredMethod(methodName, argClasses.toArray(new Class[0]));
         method.setAccessible(true);
         return (T) method.invoke(clazz, args.toArray(new Object[0]));
+    }
+
+    /**
+     * 戻り値のないprivate-staticメソッドを呼び出す。
+     *
+     * @param clazz 呼び出し対象クラス
+     * @param methodName 呼び出し対象メソッド名
+     * @param args 引数リスト
+     * @param argClasses 引数の型リスト
+     * @throws SecurityException セキュリティ・マネージャの例外
+     * @throws NoSuchMethodException 一致するメソッドが見つからない場合
+     * @throws InvocationTargetException 基本となるメソッドが例外をスローする場合
+     * @throws IllegalArgumentException メソッド引数異常の場合
+     * @throws IllegalAccessException メソッドアクセス異常の場合
+     */
+    public static void invokePrivateStaticVoidMethod(
+            Class<?> clazz,
+            String methodName,
+            List<Object> args,
+            List<Class<?>> argClasses)
+            throws NoSuchMethodException, SecurityException,
+            IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        Method method = clazz.getDeclaredMethod(methodName, argClasses.toArray(new Class[0]));
+        method.setAccessible(true);
+        method.invoke(clazz, args.toArray(new Object[0]));
     }
 
 }
