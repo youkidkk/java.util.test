c484a226d6026fc79e2ff678d04f19c1563d40ac
youkidkk <youkidkk80@gmail.com>
2016-12-18 13:09:55 +0900

    getPrivateFieldValue、getPrivateStaticFieldValue を追加

--- src/main/java/youkidkk/util/test/TestUtil.java
+++ src/main/java/youkidkk/util/test/TestUtil.java
@@ -1,5 +1,6 @@
 package youkidkk.util.test;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
@@ -20,7 +21,7 @@ public class TestUtil {
      * @param argClasses 引数の型リスト
      * @return 実行したメソッドの戻り値
      * @throws SecurityException セキュリティ・マネージャの例外
-     * @throws NoSuchMethodException 一致するメソッドが見つからない場合
+     * @throws NoSuchMethodException 対象のメソッドが見つからない場合
      * @throws InvocationTargetException 基本となるメソッドが例外をスローする場合
      * @throws IllegalArgumentException メソッド引数異常の場合
      * @throws IllegalAccessException メソッドアクセス異常の場合
@@ -49,7 +50,7 @@ public class TestUtil {
      * @param args 引数リスト
      * @param argClasses 引数の型リスト
      * @throws SecurityException セキュリティ・マネージャの例外
-     * @throws NoSuchMethodException 一致するメソッドが見つからない場合
+     * @throws NoSuchMethodException 対象のメソッドが見つからない場合
      * @throws InvocationTargetException 基本となるメソッドが例外をスローする場合
      * @throws IllegalArgumentException メソッド引数異常の場合
      * @throws IllegalAccessException メソッドアクセス異常の場合
@@ -78,7 +79,7 @@ public class TestUtil {
      * @param argClasses 引数の型リスト
      * @return 実行したメソッドの戻り値
      * @throws SecurityException セキュリティ・マネージャの例外
-     * @throws NoSuchMethodException 一致するメソッドが見つからない場合
+     * @throws NoSuchMethodException 対象のメソッドが見つからない場合
      * @throws InvocationTargetException 基本となるメソッドが例外をスローする場合
      * @throws IllegalArgumentException メソッド引数異常の場合
      * @throws IllegalAccessException メソッドアクセス異常の場合
@@ -105,7 +106,7 @@ public class TestUtil {
      * @param args 引数リスト
      * @param argClasses 引数の型リスト
      * @throws SecurityException セキュリティ・マネージャの例外
-     * @throws NoSuchMethodException 一致するメソッドが見つからない場合
+     * @throws NoSuchMethodException 対象のメソッドが見つからない場合
      * @throws InvocationTargetException 基本となるメソッドが例外をスローする場合
      * @throws IllegalArgumentException メソッド引数異常の場合
      * @throws IllegalAccessException メソッドアクセス異常の場合
@@ -121,6 +122,49 @@ public class TestUtil {
                 targetMethodName, argClasses.toArray(new Class[0]));
         method.setAccessible(true);
         method.invoke(targetClass, args.toArray(new Object[0]));
+    }
+
+    /**
+     * private変数の値を取得する。
+     *
+     * @param <T> 戻り値の型
+     * @param targetClass 対象クラス
+     * @param targetObject 対象オブジェクト
+     * @param targetFieldName 対象変数名
+     * @return 対象変数の値
+     * @throws NoSuchFieldException 対象の変数が見つからない場合
+     * @throws SecurityException セキュリティ・マネージャの例外
+     * @throws IllegalArgumentException メソッド引数異常の場合
+     * @throws IllegalAccessException メソッドアクセス異常の場合
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getPrivateFieldValue(
+            Class<?> targetClass,
+            Object targetObject,
+            String targetFieldName) throws NoSuchFieldException, SecurityException,
+            IllegalArgumentException, IllegalAccessException {
+        Field field = targetClass.getDeclaredField(targetFieldName);
+        field.setAccessible(true);
+        return (T) field.get(targetObject);
+    }
+
+    /**
+     * private-static変数の値を取得する。
+     *
+     * @param <T> 戻り値の型
+     * @param targetClass 対象クラス
+     * @param targetFieldName 対象変数名
+     * @return 対象変数の値
+     * @throws NoSuchFieldException 対象の変数が見つからない場合
+     * @throws SecurityException セキュリティ・マネージャの例外
+     * @throws IllegalArgumentException メソッド引数異常の場合
+     * @throws IllegalAccessException メソッドアクセス異常の場合
+     */
+    public static <T> T getPrivateStaticFieldValue(
+            Class<?> targetClass,
+            String targetFieldName) throws NoSuchFieldException, SecurityException,
+            IllegalArgumentException, IllegalAccessException {
+        return getPrivateFieldValue(targetClass, null, targetFieldName);
     }
 
 }
