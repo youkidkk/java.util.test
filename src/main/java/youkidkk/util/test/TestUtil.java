5913a1abbd35de51efd1bd2f6718e53475bc4771
youkidkk <youkidkk80@gmail.com>
2016-12-18 12:23:08 +0900

    パラメータの変数名とJavadocコメントの修正

--- src/main/java/youkidkk/util/test/TestUtil.java
+++ src/main/java/youkidkk/util/test/TestUtil.java
@@ -15,10 +15,10 @@ public class TestUtil {
      * @param <T> 戻り値の型
      * @param targetClass 呼び出し対象クラス
      * @param targetObject 呼び出し対象オブジェクト
-     * @param methodName 呼び出し対象メソッド名
+     * @param targetMethodName 呼び出し対象メソッド名
      * @param args 引数リスト
      * @param argClasses 引数の型リスト
-     * @return メソッドの戻り値
+     * @return 実行したメソッドの戻り値
      * @throws SecurityException セキュリティ・マネージャの例外
      * @throws NoSuchMethodException 一致するメソッドが見つからない場合
      * @throws InvocationTargetException 基本となるメソッドが例外をスローする場合
@@ -29,13 +29,13 @@ public class TestUtil {
     public static <T> T invokePrivateMethod(
             Class<?> targetClass,
             Object targetObject,
-            String methodName,
+            String targetMethodName,
             List<Object> args,
             List<Class<?>> argClasses)
             throws NoSuchMethodException, SecurityException,
             IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        Method method = targetClass.getDeclaredMethod(methodName,
-                argClasses.toArray(new Class[0]));
+        Method method = targetClass.getDeclaredMethod(
+                targetMethodName, argClasses.toArray(new Class[0]));
         method.setAccessible(true);
         return (T) method.invoke(targetObject, args.toArray(new Object[0]));
     }
@@ -45,7 +45,7 @@ public class TestUtil {
      *
      * @param targetClass 呼び出し対象クラス
      * @param targetObject 呼び出し対象オブジェクト
-     * @param methodName 呼び出し対象メソッド名
+     * @param targetMethodName 呼び出し対象メソッド名
      * @param args 引数リスト
      * @param argClasses 引数の型リスト
      * @throws SecurityException セキュリティ・マネージャの例外
@@ -57,13 +57,13 @@ public class TestUtil {
     public static void invokePrivateVoidMethod(
             Class<?> targetClass,
             Object targetObject,
-            String methodName,
+            String targetMethodName,
             List<Object> args,
             List<Class<?>> argClasses)
             throws NoSuchMethodException, SecurityException,
             IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        Method method = targetClass.getDeclaredMethod(methodName,
-                argClasses.toArray(new Class[0]));
+        Method method = targetClass.getDeclaredMethod(
+                targetMethodName, argClasses.toArray(new Class[0]));
         method.setAccessible(true);
         method.invoke(targetObject, args.toArray(new Object[0]));
     }
@@ -72,11 +72,11 @@ public class TestUtil {
      * private-staticメソッドを呼び出す。
      *
      * @param <T> 戻り値の型
-     * @param clazz 呼び出し対象クラス
-     * @param methodName 呼び出し対象メソッド名
+     * @param targetClass 呼び出し対象クラス
+     * @param targetMethodName 呼び出し対象メソッド名
      * @param args 引数リスト
      * @param argClasses 引数の型リスト
-     * @return メソッドの戻り値
+     * @return 実行したメソッドの戻り値
      * @throws SecurityException セキュリティ・マネージャの例外
      * @throws NoSuchMethodException 一致するメソッドが見つからない場合
      * @throws InvocationTargetException 基本となるメソッドが例外をスローする場合
@@ -85,22 +85,23 @@ public class TestUtil {
      */
     @SuppressWarnings("unchecked")
     public static <T> T invokePrivateStaticMethod(
-            Class<?> clazz,
-            String methodName,
+            Class<?> targetClass,
+            String targetMethodName,
             List<Object> args,
             List<Class<?>> argClasses)
             throws NoSuchMethodException, SecurityException,
             IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        Method method = clazz.getDeclaredMethod(methodName, argClasses.toArray(new Class[0]));
+        Method method = targetClass.getDeclaredMethod(
+                targetMethodName, argClasses.toArray(new Class[0]));
         method.setAccessible(true);
-        return (T) method.invoke(clazz, args.toArray(new Object[0]));
+        return (T) method.invoke(targetClass, args.toArray(new Object[0]));
     }
 
     /**
      * 戻り値のないprivate-staticメソッドを呼び出す。
      *
-     * @param clazz 呼び出し対象クラス
-     * @param methodName 呼び出し対象メソッド名
+     * @param targetClass 呼び出し対象クラス
+     * @param targetMethodName 呼び出し対象メソッド名
      * @param args 引数リスト
      * @param argClasses 引数の型リスト
      * @throws SecurityException セキュリティ・マネージャの例外
@@ -110,15 +111,16 @@ public class TestUtil {
      * @throws IllegalAccessException メソッドアクセス異常の場合
      */
     public static void invokePrivateStaticVoidMethod(
-            Class<?> clazz,
-            String methodName,
+            Class<?> targetClass,
+            String targetMethodName,
             List<Object> args,
             List<Class<?>> argClasses)
             throws NoSuchMethodException, SecurityException,
             IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        Method method = clazz.getDeclaredMethod(methodName, argClasses.toArray(new Class[0]));
+        Method method = targetClass.getDeclaredMethod(
+                targetMethodName, argClasses.toArray(new Class[0]));
         method.setAccessible(true);
-        method.invoke(clazz, args.toArray(new Object[0]));
+        method.invoke(targetClass, args.toArray(new Object[0]));
     }
 
 }
