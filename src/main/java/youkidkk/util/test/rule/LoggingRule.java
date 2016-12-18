8e44683caee993e8c7160f222bc87712d685e5f8
youkidkk <youkidkk80@gmail.com>
2016-12-18 14:31:20 +0900

    LoggingRule を追加

---
+++ src/main/java/youkidkk/util/test/rule/LoggingRule.java
@@ -0,0 +1,76 @@
+package youkidkk.util.test.rule;
+
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.MessageFormat;
+
+/**
+ * ロギングルールクラス
+ */
+public class LoggingRule extends TestWatcher {
+
+    /** メッセージパターン : 開始時 */
+    public static final String MSG_PATTERN_START = "Test Case : {0} - Start.";
+
+    /** メッセージパターン : 成功時 */
+    public static final String MSG_PATTERN_SUCCEEDED = "Test Case : {0} - Succeeded.";
+
+    /** メッセージパターン : 失敗時 */
+    public static final String MSG_PATTERN_FAILED = "Test Case : {0} - Failed.{1}{2}";
+
+    /** ロガー */
+    protected Logger logger;
+
+    /**
+     * コンストラクタ
+     */
+    public LoggingRule() {
+        this.logger = LoggerFactory.getLogger(this.getClass());
+    }
+
+    /**
+     * コンストラクタ
+     * @param logger ロガー
+     */
+    public LoggingRule(Logger logger) {
+        this.logger = logger;
+    }
+
+    /**
+     * (非 Javadoc)
+     * @see org.junit.rules.TestWatcher#starting(org.junit.runner.Description)
+     */
+    @Override
+    protected void starting(Description description) {
+        // 開始時にログを出力する
+        this.logger.info(
+                MessageFormat.format(MSG_PATTERN_START, description));
+    }
+
+    /**
+     * (非 Javadoc)
+     * @see org.junit.rules.TestWatcher#succeeded(org.junit.runner.Description)
+     */
+    @Override
+    protected void succeeded(Description description) {
+        // 成功時にログを出力する
+        this.logger.info(
+                MessageFormat.format(MSG_PATTERN_SUCCEEDED, description));
+    }
+
+    /**
+     * (非 Javadoc)
+     * @see org.junit.rules.TestWatcher#failed(java.lang.Throwable, org.junit.runner.Description)
+     */
+    @Override
+    protected void failed(Throwable th, Description description) {
+        // 失敗時にログを出力する
+        this.logger.error(
+                MessageFormat.format(MSG_PATTERN_FAILED,
+                        description, System.lineSeparator(), th.toString()));
+    }
+
+}
---
+++ src/test/java/youkidkk/util/test/rule/LoggingRuleTest.java
@@ -0,0 +1,123 @@
+package youkidkk.util.test.rule;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+import org.junit.runner.Description;
+import org.powermock.api.mockito.PowerMockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import youkidkk.util.test.TestUtil;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+
+/**
+ * ロギングルールクラス テストクラス
+ */
+public class LoggingRuleTest {
+
+    /**
+     * LoggingRule#LoggingRule() のテストメソッド
+     * {@link LoggingRule#LoggingRule()}
+     *
+     * @throws Exception 例外
+     */
+    @Test
+    public void testLoggingRule() throws Exception {
+        LoggingRule instance = new LoggingRule();
+        Logger logger = TestUtil.getPrivateFieldValue(LoggingRule.class, instance, "logger");
+        assertThat(logger.getName(), is(LoggingRule.class.getName()));
+    }
+
+    /**
+     * LoggingRule#LoggingRule(Logger) のテストメソッド
+     * {@link LoggingRule#LoggingRule(Logger)}
+     *
+     * @throws Exception 例外
+     */
+    @Test
+    public void testLoggingRuleWithLoggerArg() throws Exception {
+        Logger arglogger = LoggerFactory.getLogger(String.class);
+        LoggingRule instance = new LoggingRule(arglogger);
+        Logger logger = TestUtil.getPrivateFieldValue(LoggingRule.class, instance, "logger");
+        assertThat(logger, sameInstance(arglogger));
+    }
+
+    /** Descriptionのテスト用文字列 */
+    private static final String DESCRIPTION_STRING = "Description Test";
+
+    /** ロガーのモック */
+    private Logger loggerMock = mock(Logger.class);
+
+    /**
+     * LoggingRule#starting(Description) のテストメソッド
+     * {@link LoggingRule#starting(Description)}
+     *
+     * @throws Exception 例外
+     */
+    @Test
+    public void testStarting() throws Exception {
+        Description descriptionMock = this.getDescriptionMock();
+
+        LoggingRule instance = new LoggingRule(this.loggerMock);
+        TestUtil.invokePrivateMethod(LoggingRule.class, instance, "starting",
+                Arrays.asList(descriptionMock), Arrays.asList(Description.class));
+        String messageExpected = MessageFormat.format(LoggingRule.MSG_PATTERN_START,
+                DESCRIPTION_STRING);
+        verify(this.loggerMock, times(1)).info(messageExpected);
+    }
+
+    /**
+     * LoggingRule#succeeded(Description) のテストメソッド
+     * {@link LoggingRule#succeeded(Description)}
+     *
+     * @throws Exception 例外
+     */
+    @Test
+    public void testSucceeded() throws Exception {
+        Description descriptionMock = this.getDescriptionMock();
+
+        LoggingRule instance = new LoggingRule(this.loggerMock);
+        TestUtil.invokePrivateMethod(LoggingRule.class, instance, "succeeded",
+                Arrays.asList(descriptionMock), Arrays.asList(Description.class));
+        String messageExpected = MessageFormat.format(LoggingRule.MSG_PATTERN_SUCCEEDED,
+                DESCRIPTION_STRING);
+        verify(this.loggerMock, times(1)).info(messageExpected);
+    }
+
+    /**
+     * LoggingRule#failed(Throwable, Description) のテストメソッド
+     * {@link LoggingRule#failed(Throwable, Description)}
+     *
+     * @throws Exception 例外
+     */
+    @Test
+    public void testFailed() throws Exception {
+        Description descriptionMock = this.getDescriptionMock();
+        Exception exception = new Exception("Exception message");
+
+        LoggingRule instance = new LoggingRule(this.loggerMock);
+        TestUtil.invokePrivateMethod(LoggingRule.class, instance, "failed",
+                Arrays.asList(exception, descriptionMock),
+                Arrays.asList(Throwable.class, Description.class));
+        String messageExpected = MessageFormat.format(LoggingRule.MSG_PATTERN_FAILED,
+                DESCRIPTION_STRING, System.lineSeparator(), exception);
+        verify(this.loggerMock, times(1)).error(messageExpected);
+    }
+
+    /**
+     * Description のモックを取得する
+     *
+     * @return Description のモック
+     * @throws Exception 例外
+     */
+    private Description getDescriptionMock() throws Exception {
+        Description descriptionMock = PowerMockito.mock(Description.class);
+        PowerMockito.when(descriptionMock, "toString").thenReturn(DESCRIPTION_STRING);
+        return descriptionMock;
+    }
+
+}
