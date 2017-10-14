c88407e64ed746b434a87b9fdbbbf305940f1aa6
youkidkk <youkidkk80@gmail.com>
2017-10-14 14:17:19 +0900

    引数の型指定なくメソッド呼び出しを行うメソッドを追加

--- src/main/java/youkidkk/util/test/method/MethodUtil.java
+++ src/main/java/youkidkk/util/test/method/MethodUtil.java
@@ -2,7 +2,9 @@ package youkidkk.util.test.method;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * メソッド呼び出しユーティリティークラス
@@ -54,6 +56,27 @@ public class MethodUtil {
     }
 
     /**
+     * privateコンストラクタを呼び出す。
+     *
+     * @param <T> 戻り値の型
+     * @param targetClass 呼び出し対象クラス
+     * @param args 引数リスト
+     * @return 実行したコンストラクタの戻り値
+     * @throws Exception 例外時
+     */
+    public static <T> T invokePrivateConstructor(
+            Class<?> targetClass,
+            Object... args)
+            throws Exception {
+        if (args == null || args.length == 0) {
+            return invokePrivateConstructorWithNoArgs(targetClass);
+        } else {
+            return invokePrivateConstructorWithArgs(targetClass, Arrays.asList(args),
+                    mapToPrimitiveClassList(args));
+        }
+    }
+
+    /**
      * 引数なしのprivateメソッドを呼び出す。
      *
      * @param <T> 戻り値の型
@@ -101,6 +124,31 @@ public class MethodUtil {
     }
 
     /**
+     * privateメソッドを呼び出す。
+     *
+     * @param <T> 戻り値の型
+     * @param targetObject 呼び出し対象オブジェクト
+     * @param targetMethodName 呼び出し対象メソッド名
+     * @param args 引数リスト
+     * @return 実行したメソッドの戻り値
+     * @throws Exception 例外時
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T invokePrivateMethod(
+            Object targetObject,
+            String targetMethodName,
+            Object... args)
+            throws Exception {
+        if (args == null || args.length == 0) {
+            return (T) invokePrivateMethodWithNoArgs(targetObject.getClass(), targetObject,
+                    targetMethodName);
+        } else {
+            return (T) invokePrivateMethodWithArgs(targetObject.getClass(), targetObject,
+                    targetMethodName, Arrays.asList(args), mapToPrimitiveClassList(args));
+        }
+    }
+
+    /**
      * 戻り値のない引数なしのprivateメソッドを呼び出す。
      *
      * @param targetClass 呼び出し対象クラス
@@ -139,6 +187,28 @@ public class MethodUtil {
                 targetMethodName, argClasses.toArray(new Class[0]));
         method.setAccessible(true);
         method.invoke(targetObject, args.toArray(new Object[0]));
+    }
+
+    /**
+     * 戻り値のないprivateメソッドを呼び出す。
+     *
+     * @param targetObject 呼び出し対象オブジェクト
+     * @param targetMethodName 呼び出し対象メソッド名
+     * @param args 引数リスト
+     * @throws Exception 例外時
+     */
+    public static void invokePrivateVoidMethod(
+            Object targetObject,
+            String targetMethodName,
+            Object... args)
+            throws Exception {
+        if (args == null || args.length == 0) {
+            invokePrivateVoidMethodWithNoArgs(targetObject.getClass(), targetObject,
+                    targetMethodName);
+        } else {
+            invokePrivateVoidMethodWithArgs(targetObject.getClass(), targetObject,
+                    targetMethodName, Arrays.asList(args), mapToPrimitiveClassList(args));
+        }
     }
 
     /**
@@ -186,6 +256,30 @@ public class MethodUtil {
     }
 
     /**
+     * private-staticメソッドを呼び出す。
+     *
+     * @param <T> 戻り値の型
+     * @param targetClass 呼び出し対象クラス
+     * @param targetMethodName 呼び出し対象メソッド名
+     * @param args 引数リスト
+     * @return 実行したメソッドの戻り値
+     * @throws Exception 例外時
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T invokePrivateStaticMethod(
+            Class<?> targetClass,
+            String targetMethodName,
+            Object... args)
+            throws Exception {
+        if (args == null || args.length == 0) {
+            return (T) invokePrivateStaticMethodWithNoArgs(targetClass, targetMethodName);
+        } else {
+            return (T) invokePrivateStaticMethodWithArgs(targetClass, targetMethodName,
+                    Arrays.asList(args), mapToPrimitiveClassList(args));
+        }
+    }
+
+    /**
      * 戻り値のない引数なしのprivate-staticメソッドを呼び出す。
      *
      * @param targetClass 呼び出し対象クラス
@@ -220,6 +314,69 @@ public class MethodUtil {
                 targetMethodName, argClasses.toArray(new Class[0]));
         method.setAccessible(true);
         method.invoke(targetClass, args.toArray(new Object[0]));
+    }
+
+    /**
+     * 戻り値のないprivate-staticメソッドを呼び出す。
+     *
+     * @param targetClass 呼び出し対象クラス
+     * @param targetMethodName 呼び出し対象メソッド名
+     * @param args 引数リスト
+     * @throws Exception 例外時
+     */
+    public static void invokePrivateStaticVoidMethod(
+            Class<?> targetClass,
+            String targetMethodName,
+            Object... args)
+            throws Exception {
+        if (args == null || args.length == 0) {
+            invokePrivateStaticVoidMethodWithNoArgs(targetClass, targetMethodName);
+        } else {
+            invokePrivateStaticVoidMethodWithArgs(targetClass, targetMethodName,
+                    Arrays.asList(args), mapToPrimitiveClassList(args));
+        }
+    }
+
+    /**
+     * オブジェクト配列にプリミティブ型のラッパークラスが含まれる場合に、プリミティブ型に変換しクラス型のリストとして返却する。
+     *
+     * @param objects オブジェクト配列
+     * @return 変換後のクラス型のリスト
+     */
+    private static List<Class<?>> mapToPrimitiveClassList(Object[] objects) {
+        return Arrays.asList(objects).stream()
+                .map(arg -> mapToPrimitiveClass(arg.getClass()))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 対象のクラスがプリミティブ型のラッパークラスの場合に、プリミティブ型に変換する。
+     *
+     * @param target 対象のクラス
+     * @return 変換後クラス
+     */
+    private static Class<?> mapToPrimitiveClass(Class<?> target) {
+        Class<?> result;
+        if (target.equals(Byte.class)) {
+            result = byte.class;
+        } else if (target.equals(Short.class)) {
+            result = short.class;
+        } else if (target.equals(Integer.class)) {
+            result = int.class;
+        } else if (target.equals(Long.class)) {
+            result = long.class;
+        } else if (target.equals(Character.class)) {
+            result = char.class;
+        } else if (target.equals(Float.class)) {
+            result = float.class;
+        } else if (target.equals(Double.class)) {
+            result = double.class;
+        } else if (target.equals(Boolean.class)) {
+            result = boolean.class;
+        } else {
+            result = target;
+        }
+        return result;
     }
 
 }
